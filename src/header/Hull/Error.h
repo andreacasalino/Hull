64c9fb94e6c51d65bbc5750e21f61f8e6cd3b8b9
Andrea Casalino <andrecasa91@gmail.com>
2021-12-22 15:08:34 +0100

    init

---
+++ src/header/Hull/Error.h
@@ -0,0 +1,17 @@
+/**
+ * Author:    Andrea Casalino
+ * Created:   06.01.2020
+ *
+ * report any bug to andrecasa91@gmail.com.
+ **/
+
+#pragma once
+
+#include <stdexcept>
+
+namespace hull {
+class Error : public std::runtime_error {
+public:
+  explicit Error(const std::string &what) : std::runtime_error(what){};
+};
+} // namespace hull
